c1b2f2a260086fdeec72991725c4ba6fcda7a3a4
EMELIE\emelie <emeliehansson_@hotmail.com>
2023-09-18 16:00:11 +0200

    Added readme.md file. Reconstructured a lot of the code and put it into functions instead of keeping it all in the main loop. The functions are defined at the top of the program and then called throughout the program when needed.

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Emelies Quiz
+
+---
+
+
+### Credits:
+
+The code in the if statement in the fetchUserInput function is borrowed from the
+Adventure Game Jimmy has built during lessons.
--- run.py
+++ run.py
@@ -1,21 +1,30 @@
-# Welcome screen
-print('Are you ready to quiz? Good Luck!')
-print('---------------------------------')
+# Displays the welcome screen to the user.
+def welcomeScreen():
+    # Welcome screen
+    print('Welcome to this quiz!!')
+    print('-------------------------')
+    print('Are you ready? Good Luck!')
+    print()
 
-# Main loop/game loop
-run = True
-while run:
+# Asks the user for their name, username or what they want to give.
+def userName():
     # Ask for username
     print('Please give me your name: ')
     name = input('Name: ')
-    print(f'Hello {name}')
+    print()
+    # Prints a hello string with the users name
+    print(f'Hello, {name}')
     print()
 
+
+"""
+# Gives the user two options, here they can start the game or choose to end it.
+def startGame():
     # Ask for user input
     print('So... Are you ready?')
     print('1. For sure!')
     print("2. Don't think so..")
-    print('Make a choice by pressing A or B \n')
+    print('Make a choice by pressing 1 or 2 ')
 
     # User input
     choice = input('Answer: ')
@@ -24,139 +33,92 @@ while run:
     if not choice.isnumeric():
         print('Please provide an answer')
 
-    # Ask for user input
-    choice = int(choice)
+    # User can press 1 or 2 depending on what they want to do, start or end game.
+    choice = input('Answer: ')
     if choice == 1:
-        print('Here we go!! \n')
+        print('Here we go!! ')
     elif choice == 2:
         print('That is really sad..')
         print('Bye bye')
         run = False
     else:
-        print("That's not an option, I hope you're ready then.. \n")
+        print("That's not an option, I hope you're ready then.. ")
+"""
 
-    # Ask for user input if user wants to play with multiple choice (easier) or not.
-    print('Do you want to play with multiple choice or not? (yes/no)')
-    choice = input('Answer: ')
+
+# Displays the questions the user can choose between, user chooses inputs the number they want.
+def displayQuestions():
+    print('Choose a question: \n')
+    print('1. What is the capital of Sweden?')
+    print("2. Which female artist sings the song 'Rolling in the deep'?")
+    print('3. How many days in year?')
+    print('4. Pepsi or Coca Cola?')
+
+
+# Ask user for input.
+# the if statement in this code is borrowed from Jimmy's adventure game.
+def fetchUserInput():
+    ans = input('Answer: ')
+    print()
+    if ans.isdigit():
+        if int(ans) >= 1 and int(ans) <5:
+            return int(ans)
     print()
 
-    # Questions with multiple choice
-    if choice == 'yes' or choice == 'Yes':
-        print('Choose a question: \n')
-        print('1. What country drinks the most coffee per capita?')
-        print('2. What city is known as "The Eternal City"?')
-        print('3. What is the highest-rated film on IMDb as of September 1st, 2023?')
-        print('4. How many bones do we have in an ear?')
-        print('5. Random question')
 
-        # User input
-        choice = input('Answer: ')
+# Main program
+
+welcomeScreen()
+userName()
+
+# Main loop/game loop
+run = True
+while run:
+
+    # startGame()
+    displayQuestions()
+    choice = fetchUserInput()
+
+    choice = int(choice)
+    if choice == 1:
+        print('What is the capital of Sweden?')
+        answer = input('Answer: ')
         print()
+        if answer == 'Stockholm' or answer == 'stockholm':
+            print(f"☆ {answer} is correct! Great job! ☆ \n")
+        else:
+            print('Sorry, wrong answer.. :( \n')
 
-        choice = int(choice)
-        if choice == 1:
-            print('What country drinks the most coffee per capita?')
-            print('''
-                1. Sweden
-                2. Italy
-                3. Finland
-                4. Portugal
-            \n''')
-            answer = input('Answer: ')
-            if answer == 'Finland' or answer == 'finland' or answer == str(3):
-                print(f"{answer} is correct! Great job! \n")
-            else:
-                print('Sorry, wrong answer.. :( \n')
+    elif choice == 2:
+        print("Which female artist sings the song 'Rolling in the deep'?")
+        answer = input('Answer: ')
+        if answer == 'Adele':
+            print(f"☆ {answer} is correct! Great job! ☆ \n")
+        else:
+            print('Sorry, wrong answer.. :( \n')
 
-        if choice == 2:
-            print('What city is known as "The Eternal City"?')
-            print('''
-                1. Rome
-                2. Barcelona
-                3. Amsterdam
-                4. Berlin
-            \n''')
-            answer = input('Answer: ')
-            if answer == 'Adele' or answer == 'adele' or answer == str(1):
-                print(f"{answer} is correct! Great job! \n")
-            else:
-                print('Sorry, wrong answer.. :( \n')
-
-        if choice == 3:
-            print("What is the highest-rated film on IMDb as of September 1st, 2023?")
-            print('''
-                1. The Lord of the Rings: The Return of the King
-                2. The Godfather
-                3. Pulp Fiction
-                4. The Shawshank Redemption
-            \n''')
-            answer = input('Answer: ')
-            if answer == 'The Shawshank Redemption' or answer == 'the shawshank redemption' or answer == str(4):
-                print(f"{answer} is correct! Great job! \n")
-            else:
-                print('Sorry, wrong answer.. :( \n')
-
-        if choice == 4:
-            print('How many bones do we have in an ear?')
-            print('''
-                1. Seven
-                2. Three
-                3. Four
-                4. Six
-            \n''')
-            answer = input('Answer: ')
-            if answer == 'Three' or answer == 'three' or answer == str(2):
-                print(f"{answer} is correct! Great job! \n")
-            else:
-                print('Sorry, wrong answer.. :( \n')
-        continue
-
-    # Quiz without multiple choice, the user has to write out the answer
-    if choice == 'no' or choice == 'No':
-        # Quiz questions
-        print('Choose a question: \n')
-        print('1. What is the capital of Sweden?')
-        print("2. Which female artist sings the song 'Rolling in the deep'?")
-        print('3. How many days in year?')
-        print('4. Pepsi or Coca Cola?')
-
-        # User input
-        choice = input('Answer: ')
+    elif choice == 3:
+        print("How many days in year'?")
+        answer = input('Answer: ')
         print()
+        if answer == str(365):
+            print(f"☆ {answer} is correct! Great job! ☆ \n")
+        else:
+            print('Sorry, wrong answer.. :( \n')
 
-        choice = int(choice)
-        if choice == 1:
-            print('What is the capital of Sweden?')
-            answer = input('Answer: ')
-            if answer == 'Stockholm' or answer == 'stockholm':
-                print(f"{answer} is correct! Great job! \n")
-            else:
-                print('Sorry, wrong answer.. :( \n')
+    elif choice == 4:
+        print('Pepsi or Coca Cola?')
+        answer = input('Answer: ')
+        print()
+        if answer == 'Pepsi' or answer == 'pepsi':
+            print(f"☆ That's correct! {answer} always wins! ☆ \n")
+        else:
+            print('Sorry, wrong answer.. :( Pepsi is always the winner! \n')
 
-        if choice == 2:
-            print("Which female artist sings the song 'Rolling in the deep'?")
-            answer = input('Answer: ')
-            if answer == 'Adele':
-                print(f"{answer} is correct! Great job! \n")
-            else:
-                print('Sorry, wrong answer.. :( \n')
+    else:
+        print('Sorry, I cannot understand what you want to do.')
 
-        if choice == 3:
-            print("How many days in year'?")
-            answer = input('Answer: ')
-            if answer == str(365):
-                print(f"{answer} is correct! Great job! \n")
-            else:
-                print('Sorry, wrong answer.. :( \n')
-
-        if choice == 4:
-            print('Pepsi or Coca Cola?')
-            answer = input('Answer: ')
-            if answer == 'Pepsi':
-                print(f"That's correct! {answer} always wins! \n")
-            else:
-                print('Sorry, wrong answer.. :( Pepsi is always the winner! \n')
-        continue
+displayQuestions()
 
 
 
